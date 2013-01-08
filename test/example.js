7d5a6131ecf6df59084cd636a7d6887789a207d0
Takuto Wada <takuto.wada@gmail.com>
2013-01-08 20:41:35 +0900

    only store columns

--- test/example.js
+++ test/example.js
@@ -1,14 +1,29 @@
 var q = require('qunitjs'),
     util = require('util');
 
-var __current__ = {};
-var __save__ = function (name, value, loc) {
-    __current__[name] = {
+var __current__ = [];
+var __save__ = function (name, value, start, end) {
+    __current__.push({
         name: name,
         value: value,
-        loc: loc
-    };
+        start: start,
+        end: end
+        //loc: loc
+    });
     return value;
+};
+var __assert__ = function (result, line) {
+    if (!result) {
+        //q.tap.note('failed: ' + line);
+        q.tap.note(line);
+        var str = '';
+        for(var i = 0; i < line.length; i += 1) {
+            str += line.charAt(i);
+        }
+        // line.length
+        q.tap.note(q.tap.explain(__current__));
+    }
+    __current__ = [];
 };
 
 (function (qunitObject) {
@@ -16,15 +31,6 @@ var __save__ = function (name, value, loc) {
     qunitTap(qunitObject, util.puts, {noPlan: true, showSourceOnFailure: true});
     qunitObject.init();
     qunitObject.config.updateRate = 0;
-    var tap = qunitObject.tap;
-
-	// log: { result, actual, expected, message }
-    qunitObject.log(function (details) {
-        if(!(details.result)) {
-            tap.note(tap.explain(__current__));
-        }
-        __current__ = {};
-    });
 })(q);
 
 q.test('spike', function (assert) {
