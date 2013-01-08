eb7a03667329c6aaaae5a9424e9d72957e1aa7aa
Takuto Wada <takuto.wada@gmail.com>
2013-01-08 23:37:06 +0900

    Extract generator

--- gen.js
+++ gen.js
@@ -1,6 +1,6 @@
 /*jslint node:true */
 var fs = require('fs'),
-    passert = require('./lib/power-assert'),
+    gen = require('./lib/power-assert-gen'),
     file = process.argv[2];
 var content = fs.readFileSync(file, 'utf-8');
-passert(content);
+gen(content);
