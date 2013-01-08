737f1aff1b3fa02cda04e1280dfd3166e0471368
Takuto Wada <takuto.wada@gmail.com>
2013-01-09 02:29:03 +0900

    moved to bin

--- bin/pagen.js
+++ bin/pagen.js
@@ -1,5 +1,5 @@
 /*jslint node:true */
-var generate = require('./lib/power-assert-gen'),
+var generate = require('../lib/power-assert-gen'),
     fs = require('fs'),
     file = process.argv[2];
 generate(fs.readFileSync(file, 'utf-8'), {module: 'CommonJS'});
