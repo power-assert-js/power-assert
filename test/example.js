dd80605a54a3f9c7f92c3e2e872b43f729e3e161
Takuto Wada <takuto.wada@gmail.com>
2013-01-09 00:06:49 +0900

    sample does not require qunit-tap

--- test/example.js
+++ test/example.js
@@ -1,12 +1,4 @@
-var q = require('qunitjs'),
-    util = require('util');
-
-(function (qunitObject) {
-    var qunitTap = require("qunit-tap").qunitTap;
-    qunitTap(qunitObject, util.puts, {noPlan: true, showSourceOnFailure: true});
-    qunitObject.init();
-    qunitObject.config.updateRate = 0;
-})(q);
+var q = require('qunitjs');
 
 q.test('spike', function (assert) {
     var hoge = 'foo';
