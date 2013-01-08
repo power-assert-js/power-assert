4f416248c3b155632138540f63a7d3e03e86482e
Takuto Wada <takuto.wada@gmail.com>
2013-01-08 22:17:13 +0900

    prettify

--- test/example.js
+++ test/example.js
@@ -57,6 +57,7 @@ var __assert__ = function (result, line) {
     buffers.forEach(function (buffer, index) {
         q.tap.note(buffer.join(''));
     });
+    q.tap.note('');
 
     __current__ = [];
 };
@@ -72,4 +73,11 @@ q.test('spike', function (assert) {
     var hoge = 'foo';
     var fuga = 'bar';
     assert.ok(hoge === fuga, 'comment');
+
+    var piyo = 3;
+    assert.ok(fuga === piyo);
+
+    var longString = 'very very loooooooooooooooooooooooooooooooooooooooooooooooooooong message';
+    var anotherLongString = 'yet another loooooooooooooooooooooooooooooooooooooooooooooooooooong message';
+    assert.ok(longString === anotherLongString);
 });
