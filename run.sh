4d359647761c73e5ab309869f6354cbff8caec78
Takuto Wada <takuto.wada@gmail.com>
2013-01-09 16:52:07 +0900

    rename example

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./bin/empower.js sandbox/example.js > sandbox/example_pa.js && node sandbox/example_pa.js
+./bin/empower.js sandbox/qunit_test.js > sandbox/qunit_test_empowered.js && node sandbox/qunit_test_empowered.js
 
-./bin/empower.js sandbox/plain_node.js > sandbox/plain_node_pa.js && node sandbox/plain_node_pa.js
+./bin/empower.js sandbox/plain_node.js > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
