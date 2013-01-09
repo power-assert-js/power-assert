011b4090eb4e17814b0f8943dcf863f378855a2a
Takuto Wada <takuto.wada@gmail.com>
2013-01-10 01:19:34 +0900

    renamed

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./bin/empower.js sandbox/qunit_test_rhino.js > sandbox/qunit_test_rhino_empowered.js && java -jar js.jar rhino.js
-./bin/empower.js sandbox/qunit_test.js --node > sandbox/qunit_test_empowered.js && node sandbox/qunit_test_empowered.js
-# ./bin/empower.js sandbox/plain_node.js --node > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
+./bin/empower.js sandbox/qunit_rhino.js > sandbox/qunit_rhino_empowered.js && java -jar js.jar rhino.js
+./bin/empower.js sandbox/qunit_node.js --node > sandbox/qunit_node_empowered.js && node sandbox/qunit_node_empowered.js
+./bin/empower.js sandbox/plain_node.js --node > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
