aff2a3688c4a87dd57895d1a982c6141c066b6f1
Takuto Wada <takuto.wada@gmail.com>
2013-01-09 12:05:01 +0900

    organizing tests

--- test/pagen_test.js
+++ test/pagen_test.js
@@ -13,25 +13,36 @@ var pagen = require('../lib/power-assert-gen'),
     qu.config.updateRate = 0;
 })(q);
 
-var extractExpressionFrom = function (string) {
-    var tree = esprima.parse(string, {tolerant: true, loc: true}),
-        expressionStatement = tree.body[0],
-        expression = expressionStatement.expression;
-    return expression;
-};
+var instr = (function () {
+    var extractExpressionFrom = function (string) {
+        var tree = esprima.parse(string, {tolerant: true, loc: true}),
+            expressionStatement = tree.body[0],
+            expression = expressionStatement.expression;
+        return expression;
+    };
+    return function (before, after) {
+        q.test(before, function (assert) {
+            var line = before,
+                expression = extractExpressionFrom(line);
+            pagen.instrument(expression, line);
+            assert.equal(escodegen.generate(expression), after);
+        });
+    };
+})();
 
 q.module('instrument');
 
-q.test('assert(fuga === piyo);', function (assert) {
-    var line = q.config.current.testName,
-        expression = extractExpressionFrom(line);
-    pagen.instrument(expression, line);
-    assert.equal(escodegen.generate(expression), "assert(passert.__expr__(passert.__ident__('fuga', fuga, 7, 11) === passert.__ident__('piyo', piyo, 16, 20), 'assert(fuga === piyo);'))");
-});
+instr(
+    'assert(fuga === piyo);',
+    "assert(passert.__expr__(passert.__ident__('fuga', fuga, 7, 11) === passert.__ident__('piyo', piyo, 16, 20), 'assert(fuga === piyo);'))"
+);
 
-q.test('assert.ok(fuga === piyo);', function (assert) {
-    var line = q.config.current.testName,
-        expression = extractExpressionFrom(line);
-    pagen.instrument(expression, line);
-    assert.equal(escodegen.generate(expression), "assert.ok(passert.__expr__(passert.__ident__('fuga', fuga, 10, 14) === passert.__ident__('piyo', piyo, 19, 23), 'assert.ok(fuga === piyo);'))");
-});
+instr(
+    'assert.ok(fuga === piyo);',
+    "assert.ok(passert.__expr__(passert.__ident__('fuga', fuga, 10, 14) === passert.__ident__('piyo', piyo, 19, 23), 'assert.ok(fuga === piyo);'))"
+);
+
+instr(
+    'assert.ok(4 !== 4);',
+    "assert.ok(passert.__expr__(4 !== 4, 'assert.ok(4 !== 4);'))"
+);
