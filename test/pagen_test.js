da6c1f9c4602980300f6d48d56ccde5d24c975eb
Takuto Wada <takuto.wada@gmail.com>
2013-01-09 12:46:03 +0900

    tests

--- test/pagen_test.js
+++ test/pagen_test.js
@@ -30,19 +30,69 @@ var instr = (function () {
     };
 })();
 
-q.module('instrument');
+
+
+q.module('Identifier');
+
+instr(
+    'assert(falsyStr);',
+    "assert(passert.__expr__(passert.__ident__('falsyStr', falsyStr, 7, 15), 'assert(falsyStr);'))"
+);
+
+instr(
+    'assert.ok(falsyStr);',
+    "assert.ok(passert.__expr__(passert.__ident__('falsyStr', falsyStr, 10, 18), 'assert.ok(falsyStr);'))"
+);
+
+
+
+q.module('BinaryExpression with Identifier');
 
 instr(
     'assert(fuga === piyo);',
     "assert(passert.__expr__(passert.__ident__('fuga', fuga, 7, 11) === passert.__ident__('piyo', piyo, 16, 20), 'assert(fuga === piyo);'))"
 );
-
 instr(
     'assert.ok(fuga === piyo);',
     "assert.ok(passert.__expr__(passert.__ident__('fuga', fuga, 10, 14) === passert.__ident__('piyo', piyo, 19, 23), 'assert.ok(fuga === piyo);'))"
 );
+instr(
+    'assert(fuga !== piyo);',
+    "assert(passert.__expr__(passert.__ident__('fuga', fuga, 7, 11) !== passert.__ident__('piyo', piyo, 16, 20), 'assert(fuga !== piyo);'))"
+);
+instr(
+    'assert.ok(fuga !== piyo);',
+    "assert.ok(passert.__expr__(passert.__ident__('fuga', fuga, 10, 14) !== passert.__ident__('piyo', piyo, 19, 23), 'assert.ok(fuga !== piyo);'))"
+);
 
+instr(
+    'assert(fuga !== 4);',
+    "assert(passert.__expr__(passert.__ident__('fuga', fuga, 7, 11) !== 4, 'assert(fuga !== 4);'))"
+);
+instr(
+    'assert.ok(fuga !== 4);',
+    "assert.ok(passert.__expr__(passert.__ident__('fuga', fuga, 10, 14) !== 4, 'assert.ok(fuga !== 4);'))"
+);
+
+instr(
+    'assert(4 !== 4);',
+    "assert(passert.__expr__(4 !== 4, 'assert(4 !== 4);'))"
+);
 instr(
     'assert.ok(4 !== 4);',
     "assert.ok(passert.__expr__(4 !== 4, 'assert.ok(4 !== 4);'))"
+);
+
+
+
+q.module('BinaryExpression with MemberExpression');
+
+instr(
+    'assert(ary1.length === ary2.length);',
+    "assert(passert.__expr__(passert.__ident__('length', passert.__ident__('ary1', ary1, 7, 11).length, 12, 18) === passert.__ident__('length', passert.__ident__('ary2', ary2, 23, 27).length, 28, 34), 'assert(ary1.length === ary2.length);'))"
+);
+
+instr(
+    'assert.ok(ary1.length === ary2.length);',
+    "assert.ok(passert.__expr__(passert.__ident__('length', passert.__ident__('ary1', ary1, 10, 14).length, 15, 21) === passert.__ident__('length', passert.__ident__('ary2', ary2, 26, 30).length, 31, 37), 'assert.ok(ary1.length === ary2.length);'))"
 );
