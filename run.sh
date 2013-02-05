a1e089e17092575deecda542aec09eea1fad62f6
Takuto Wada <takuto.wada@gmail.com>
2013-02-05 11:20:47 +0900

    verbose output

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+echo '##################################'
+echo '##### qunit_node #################'
+echo '##################################'
 ./bin/empower.js --module=commonjs sandbox/qunit_node.js > sandbox/qunit_node_empowered.js && node sandbox/qunit_node_empowered.js
+
+echo '##################################'
+echo '##### plain_node #################'
+echo '##################################'
 ./bin/empower.js --module=commonjs sandbox/plain_node.js > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
 
 ./bin/empower.js sandbox/qunit_rhino.js > sandbox/qunit_rhino_empowered.js
+
+echo '##################################'
+echo '##### phantomjs_test #############'
+echo '##################################'
 ./sandbox/phantomjs_test.sh
+
+echo '##################################'
+echo '##### run_rhino ##################'
+echo '##################################'
 java -jar js.jar sandbox/run_rhino.js
