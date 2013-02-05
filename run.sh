03463d2134ef755058af0212a0e8194347fc272c
Takuto Wada <takuto.wada@gmail.com>
2013-02-05 14:43:27 +0900

    Remove suffix from command

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 echo '##################################'
 echo '##### qunit_node #################'
 echo '##################################'
-./bin/empower.js --module=commonjs sandbox/qunit_node.js > sandbox/qunit_node_empowered.js && node sandbox/qunit_node_empowered.js
+./bin/empower --module=commonjs sandbox/qunit_node.js > sandbox/qunit_node_empowered.js && node sandbox/qunit_node_empowered.js
 
 echo '##################################'
 echo '##### plain_node #################'
 echo '##################################'
-./bin/empower.js --module=commonjs sandbox/plain_node.js > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
+./bin/empower --module=commonjs sandbox/plain_node.js > sandbox/plain_node_empowered.js && node sandbox/plain_node_empowered.js
 
-./bin/empower.js sandbox/qunit_rhino.js > sandbox/qunit_rhino_empowered.js
+./bin/empower sandbox/qunit_rhino.js > sandbox/qunit_rhino_empowered.js
 
 echo '##################################'
 echo '##### phantomjs_test #############'
