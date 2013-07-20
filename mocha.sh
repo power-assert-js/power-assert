b307e61bfcd49368689c911817f0ee5e5247fa08
Takuto Wada <takuto.wada@gmail.com>
2013-07-20 16:19:02 +0900

    Using mocha min reporter

--- mocha.sh
+++ mocha.sh
@@ -3,4 +3,4 @@
 echo '##################################'
 echo '##### mocha_node #################'
 echo '##################################'
-./bin/empower sandbox/mocha_node.js > sandbox/mocha_node_empowered.js && ./node_modules/.bin/mocha sandbox/mocha_node_empowered.js
+./bin/empower sandbox/mocha_node.js > sandbox/mocha_node_empowered.js && ./node_modules/.bin/mocha --reporter min sandbox/mocha_node_empowered.js
