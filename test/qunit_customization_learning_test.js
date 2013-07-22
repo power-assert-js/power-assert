8de586fe4ed71fd1c16fa162acf89b0ba6e83f26
Takuto Wada <takuto.wada@gmail.com>
2013-07-23 02:19:44 +0900

    More learning tests for QUnit.

--- test/qunit_customization_learning_test.js
+++ test/qunit_customization_learning_test.js
@@ -16,7 +16,17 @@ var q = require('qunitjs'),
     qunit['notOk'] = notOk;
 })(q);
 
+
 q.test('add notOk function', function (assert) {
     assert.notOk(false);
     q.notOk(null);
+});
+
+q.test('test in test', function (assert) {
+    q.test('nested', function (assert) {
+        assert.notOk(false, 'foo');
+        q.notOk(null, 'bar');
+    });
+
+    assert.notOk(false, 'baz');
 });
