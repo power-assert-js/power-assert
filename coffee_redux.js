353eb72060f7e68e4f2156c0e2ea498123aa1593
Takuto Wada <takuto.wada@gmail.com>
2013-07-23 20:20:55 +0900

    Add CoffeeScriptRedux integration example.

---
+++ coffee_redux.js
@@ -0,0 +1,19 @@
+var empower = require('./lib/empower'),
+    CoffeeScript = require('coffee-script-redux'),
+    argv = require('optimist').argv,
+    fs = require('fs'),
+    file = argv._[0],
+    path = fs.realpathSync(file),
+    csCode = fs.readFileSync(file, 'utf-8');
+
+var parseOptions = {raw: true};
+var compileOptions = {bare: true};
+var jsGenerateOptions = {compact: true};
+var empowerOptions = {destructive: false, source: csCode, path: path, powerAssertVariableName: 'assert'};
+
+var csAST = CoffeeScript.parse(csCode, parseOptions);
+var jsAST = CoffeeScript.compile(csAST, compileOptions);
+var empoweredAst = empower(jsAST, empowerOptions);
+var jsCode = CoffeeScript.js(empoweredAst, jsGenerateOptions);
+
+console.log(jsCode);
--- run.sh
+++ run.sh
@@ -25,4 +25,20 @@ java -jar js.jar sandbox/run_rhino.js
 echo '##################################'
 echo '##### mocha_node #################'
 echo '##################################'
-./bin/empower sandbox/mocha_node.js > sandbox/mocha_node_empowered.js && ./node_modules/.bin/mocha sandbox/mocha_node_empowered.js
+./bin/empower sandbox/mocha_node.js > sandbox/mocha_node_empowered.js && ./node_modules/.bin/mocha --reporter tap sandbox/mocha_node_empowered.js
+
+
+echo '##################################'
+echo '##### coffee script redux ########'
+echo '##################################'
+node ./coffee_redux.js sandbox/dog.test.coffee > sandbox/dog.test.empowered.js && ./sandbox/phantomjs_coffee.sh
+
+echo '#######################################'
+echo '##### coffee script redux Node ########'
+echo '#######################################'
+node ./coffee_redux.js sandbox/dog.test.node.coffee > sandbox/dog.test.node.empowered.js && node sandbox/dog.test.node.empowered.js
+
+echo '###############################################'
+echo '##### coffee script redux Node & Mocha ########'
+echo '###############################################'
+node ./coffee_redux.js sandbox/mocha_node.coffee > sandbox/mocha_node.coffee.empowered.js && ./node_modules/.bin/mocha --reporter tap sandbox/mocha_node.coffee.empowered.js
