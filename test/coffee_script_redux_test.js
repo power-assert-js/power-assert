d1e2bde448a555a00e31ada83364711a301b72bb
Takuto Wada <takuto.wada@gmail.com>
2013-07-24 15:09:02 +0900

    Simplicity: do not pass formatter around.

--- test/coffee_script_redux_test.js
+++ test/coffee_script_redux_test.js
@@ -2,8 +2,8 @@ var q = require('../test_helper').QUnit,
     formatter = require('../lib/power-assert-formatter'),
     enhancer = require('../lib/power-assert-core'),
     powerAssertTextLines = [],
-    _pa_ = enhancer(null, formatter, function (powerOk, context, message, powerAssertText) {
-        powerAssertTextLines = powerAssertText.split('\n');
+    _pa_ = enhancer(q.assert, function (powerOk, context, message) {
+        powerAssertTextLines = formatter.format(context);
     }),
     empower = require('../lib/empower'),
     esprima = require('esprima'),
