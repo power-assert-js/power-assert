ab287bcfada46a0af6bd5761ebd1d3cf3e1d797f
Takuto Wada <takuto.wada@gmail.com>
2013-08-21 08:28:44 +0900

    Fix example runner script

--- scripts/example_runners/qunit_node_coffee.sh
+++ scripts/example_runners/qunit_node_coffee.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-node ./scripts/coffee_redux.js sandbox/dog.test.coffee > sandbox/dog.test.espowered.js && ./sandbox/phantomjs_coffee.sh
+node ./scripts/coffee_redux.js sandbox/dog.test.node.coffee > sandbox/dog.test.espowered.js && node sandbox/dog.test.espowered.js
 exit 0
