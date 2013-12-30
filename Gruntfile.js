930706375c8d4428d8204c062764ec49ebef996a
Takuto Wada <takuto.wada@gmail.com>
2013-12-30 10:55:08 +0900

    Use load-grunt-tasks module to load grunt tasks easily.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,7 @@
 module.exports = function(grunt) {
     var pkg = grunt.file.readJSON('package.json');
 
-    (function () {
-        var taskName;
-        for(taskName in pkg.devDependencies) {
-            if(taskName.substring(0, 6) === 'grunt-') {
-                grunt.loadNpmTasks(taskName);
-            }
-        }
-    })();
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         pkg: pkg,
