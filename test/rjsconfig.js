4f1aa7ce0b160c117726884305a97dd4f7e03e4c
Takuto Wada <takuto.wada@gmail.com>
2014-03-24 15:51:17 +0900

    Fix expect.js require path.

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -2,7 +2,7 @@ var require = {
     paths: {
         assert: "../bower_components/assert/assert",
         "es5-shim": "../bower_components/es5-shim/es5-shim",
-        expect: "../bower_components/expect/expect",
+        expect: "../bower_components/expect/index",
         "power-assert-formatter": "../bower_components/power-assert-formatter/lib/power-assert-formatter",
         "qunit-tap": "../bower_components/qunit-tap/lib/qunit-tap",
         qunit: "../bower_components/qunit/qunit/qunit",
