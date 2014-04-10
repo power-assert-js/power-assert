832375d4ddca723b26ec111949a6826613b27e59
Takuto Wada <takuto.wada@gmail.com>
2014-04-10 15:05:39 +0900

    Use tap reporter for prove

--- scripts/example_runners/mocha_node_js.sh
+++ scripts/example_runners/mocha_node_js.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 MOCHA=./node_modules/grunt-mocha-test/node_modules/.bin/mocha
-$MOCHA --reporter spec --require './sandbox/loader' sandbox/mocha_node.js
+$MOCHA --reporter tap --require './sandbox/loader' sandbox/mocha_node.js
 
 exit 0
