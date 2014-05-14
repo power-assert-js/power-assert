7240a3c8ef90a31ffcd5d4d5c30b1d85b0efdfd2
Takuto Wada <takuto.wada@gmail.com>
2014-05-15 01:56:59 +0900

    Browser side test by gulp-mocha-phantomjs.

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -1,7 +1,6 @@
 var require = {
     paths: {
         assert: "../bower_components/assert/assert",
-        "es5-shim": "../bower_components/es5-shim/es5-shim",
         expect: "../bower_components/expect/index",
         "power-assert-formatter": "../bower_components/power-assert-formatter/lib/power-assert-formatter",
         "qunit-tap": "../bower_components/qunit-tap/lib/qunit-tap",
