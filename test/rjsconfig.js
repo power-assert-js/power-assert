6fa17eb4e984c6a05a0eca6d0b41400b5926ff1a
Takuto Wada <takuto.wada@gmail.com>
2014-05-19 16:11:55 +0900

    Remove unused dependencies from AMD testing.

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -2,12 +2,7 @@ var require = {
     paths: {
         "power-assert": "../build/power-assert",
         expect: "../bower_components/expect/index",
-        "power-assert-formatter": "../bower_components/power-assert-formatter/lib/power-assert-formatter",
-        "qunit-tap": "../bower_components/qunit-tap/lib/qunit-tap",
-        qunit: "../bower_components/qunit/qunit/qunit",
         mocha: "../bower_components/mocha/mocha",
-        empower: "../bower_components/empower/lib/empower",
-        esprima: "../bower_components/esprima/esprima",
         requirejs: "../bower_components/requirejs/require"
     },
     shim: {
