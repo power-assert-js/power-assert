5591f01209f782751b38945552f495b0fd42b869
Takuto Wada <takuto.wada@gmail.com>
2014-05-19 16:29:43 +0900

    Update mocha and sandbox example

--- scripts/example_runners/mocha_node_js.sh
+++ scripts/example_runners/mocha_node_js.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MOCHA=./node_modules/grunt-mocha-test/node_modules/.bin/mocha
+MOCHA=./node_modules/.bin/mocha
 $MOCHA --reporter tap --require './sandbox/loader' sandbox/mocha_node.js
 
 exit 0
