f033773bfd041aa5b9ac86900dc9b7030bcb7777
Takuto Wada <takuto.wada@gmail.com>
2014-05-19 16:30:14 +0900

    Use espower-source in espower-cli.js

--- scripts/espower-cli.js
+++ scripts/espower-cli.js
@@ -7,21 +7,15 @@
  * Licensed under the MIT license.
  *   https://raw.github.com/twada/power-assert/master/MIT-LICENSE.txt
  */
-var espower = require('espower'),
-    esprima = require('esprima'),
-    escodegen = require('escodegen'),
+var espowerSourceToSource = require('espower-source'),
     argv = require('optimist').argv,
     fs = require('fs'),
     options = {},
     file = argv._[0],
     path = fs.realpathSync(file),
-    source = fs.readFileSync(file, 'utf-8'),
-    tree = esprima.parse(source, {tolerant: true, loc: true, tokens: true, raw: true, source: path});
+    code = fs.readFileSync(file, 'utf-8');
 
 if (argv.powerAssertVariableName) {
-  options['powerAssertVariableName'] = argv.powerAssertVariableName;
+    options['powerAssertVariableName'] = argv.powerAssertVariableName;
 }
-options['path'] = path;
-options['source'] = source;
-tree = espower(tree, options);
-console.log(escodegen.generate(tree));
+console.log(espowerSourceToSource(code, path, options));
