172882da50fe3413f047d1fa94ab28ee5a0c6019
Takuto Wada <takuto.wada@gmail.com>
2014-05-29 13:50:05 +0900

    Refine gulp config a bit

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,12 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     browserify = require('browserify'),
     merge = require('lodash.merge'),
-    paths = {
+    config = {
         bump: {
             target: ['./bower.json', './package.json']
         },
         bundle: {
+            standalone: 'assert',
             srcFile: './lib/power-assert.js',
             destDir: './build',
             destName: 'power-assert.js'
@@ -48,26 +49,26 @@ function runMocha(pattern, extraOptions) {
 }
 
 function runMochaWithEspowerLoader() {
-    return runMocha(paths.test.base + paths.test.pattern, {r: './enable_power_assert'});
+    return runMocha(config.test.base + config.test.pattern, {r: './enable_power_assert'});
 }
 
 function bumpVersion(options) {
     return gulp
-        .src(paths.bump.target)
+        .src(config.bump.target)
         .pipe(bump(options))
         .pipe(gulp.dest('./'));
 }
 
 gulp.task('git_add', function(){
     return gulp
-        .src(paths.bump.target)
+        .src(config.bump.target)
         .pipe(git.add());
 });
 
 gulp.task('git_commit', function(){
     var pkg = require('./package.json');
     return gulp
-        .src(paths.bump.target)
+        .src(config.bump.target)
         .pipe(git.commit(pkg.version));
 });
 
@@ -105,34 +106,34 @@ gulp.task('watch', function () {
 
 gulp.task('clean_bundle', function () {
     return gulp
-        .src(paths.bundle.destDir, {read: false})
+        .src(config.bundle.destDir, {read: false})
         .pipe(clean());
 });
 
 gulp.task('bundle', function() {
-    var bundleStream = browserify(paths.bundle.srcFile).bundle({standalone: 'assert'});
+    var bundleStream = browserify(config.bundle.srcFile).bundle({standalone: config.bundle.standalone});
     return bundleStream
-        .pipe(source(paths.bundle.destName))
-        .pipe(gulp.dest(paths.bundle.destDir));
+        .pipe(source(config.bundle.destName))
+        .pipe(gulp.dest(config.bundle.destDir));
 })
 
 gulp.task('clean_espower', function () {
     return gulp
-        .src(paths.test.poweredDir, {read: false})
+        .src(config.test.poweredDir, {read: false})
         .pipe(clean());
 });
 
 gulp.task('copy_others', function(){
     return gulp
-        .src(paths.test.notToBeInstrumented(), {base: paths.test.base})
-        .pipe(gulp.dest(paths.test.poweredDir));
+        .src(config.test.notToBeInstrumented(), {base: config.test.base})
+        .pipe(gulp.dest(config.test.poweredDir));
 });
 
 gulp.task('espower', function() {
     return gulp
-        .src(paths.test.toBeInstrumented(), {base: paths.test.base})
+        .src(config.test.toBeInstrumented(), {base: config.test.base})
         .pipe(espower())
-        .pipe(gulp.dest(paths.test.poweredDir));
+        .pipe(gulp.dest(config.test.poweredDir));
 });
 
 gulp.task('unit', function () {
@@ -140,18 +141,18 @@ gulp.task('unit', function () {
 });
 
 gulp.task('test_generated', function () {
-    return runMocha(paths.test.poweredDir + paths.test.pattern);
+    return runMocha(config.test.poweredDir + config.test.pattern);
 });
 
 gulp.task('test_amd', function () {
     return gulp
-        .src(paths.test.amd)
+        .src(config.test.amd)
         .pipe(mochaPhantomJS({reporter: 'dot'}));
 });
 
 gulp.task('test_browser', function () {
     return gulp
-        .src(paths.test.browser)
+        .src(config.test.browser)
         .pipe(mochaPhantomJS({reporter: 'dot'}));
 });
 
