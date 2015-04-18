2f578c973e4738e56cf4e1f5702038986367214d
Takuto Wada <takuto.wada@gmail.com>
2015-04-18 19:34:36 +0900

    chore(power-assert): use Babel to transform tests including ES6

--- enable_power_assert.js
+++ enable_power_assert.js
@@ -1,16 +1,5 @@
-require('espower-loader')({
-    cwd: process.cwd(),
-    pattern: 'test/tobe_instrumented/*_test.js',
-    espowerOptions: {
-        patterns: [
-            'assert(value, [message])',
-            'assert.ok(value, [message])',
-            'assert.equal(actual, expected, [message])',
-            'assert.notEqual(actual, expected, [message])',
-            'assert.strictEqual(actual, expected, [message])',
-            'assert.notStrictEqual(actual, expected, [message])',
-            'assert.deepEqual(actual, expected, [message])',
-            'assert.notDeepEqual(actual, expected, [message])'
-        ]
-    }
+require("babel/register")({
+    only: /test\/tobe_instrumented/,
+    plugins: ['babel-plugin-espower'],
+    extensions: ['.es6', '.js']
 });
