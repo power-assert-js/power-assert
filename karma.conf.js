d9f621bacbb77c7314dce14e770c2ff06538702b
falsandtru <falsandtru@users.noreply.github.com>
2016-04-25 18:31:01 +0900

    test(power-assert): use shared espowered tests

--- karma.conf.js
+++ karma.conf.js
@@ -4,18 +4,10 @@ module.exports = function (config) {
         frameworks: ['mocha', 'expect'],
         files: [
             { pattern: 'build/power-assert.js', watched: true, served: true, included: true },
-            { pattern: 'test/tobe_instrumented/power_assert_test.js', watched: true, served: true, included: true },
-            { pattern: 'test/tobe_instrumented/es6_test.js', watched: true, served: true, included: true },
-            { pattern: 'test/not_tobe_instrumented/not_instrumented_test.js', watched: true, served: true, included: true }
+            { pattern: 'espowered_tests/tobe_instrumented/power_assert_test.js', watched: true, served: true, included: true },
+            { pattern: 'espowered_tests/tobe_instrumented/es6_test.js', watched: true, served: true, included: true },
+            { pattern: 'espowered_tests/not_tobe_instrumented/not_instrumented_test.js', watched: true, served: true, included: true }
         ],
-        preprocessors: {
-            'test/*/*.js': ['espower']
-        },
-        espowerPreprocessor: {
-            options: {
-                emitActualCode: false
-            }
-        },
         reporters: ['dots'],
         port: 9876,
         colors: true,
