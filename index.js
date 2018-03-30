0d24c6e458ec1879e45427696db9aba71f325214
Takuto Wada <takuto.wada@gmail.com>
2018-03-30 12:57:36 +0900

    chore: update copyright year

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *
  * https://github.com/power-assert-js/power-assert
  *
- * Copyright (c) 2013-2017 Takuto Wada
+ * Copyright (c) 2013-2018 Takuto Wada
  * Licensed under the MIT license.
  *   https://github.com/power-assert-js/power-assert/blob/master/MIT-LICENSE.txt
  */
